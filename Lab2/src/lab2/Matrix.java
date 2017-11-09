7eea7ff82a1d668cbfdee4370b3469fdcdb15ff5
Maja Gruca <majag@mops.uci.agh.edu.pl>
2017-11-09 14:39:42 +0100

    Kolokwium 1

---
+++ Lab2/src/lab2/Matrix.java
@@ -0,0 +1,276 @@
+package lab2;
+
+public class Matrix {
+    double[] data;
+    int rows;
+    int cols;
+
+    Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = new double[rows * cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                data[i * cols + j] = 0;
+            }
+        }
+    }
+
+    Matrix(double[][] d) {
+        rows = d.length;
+        int i = 0;
+        int max_col = 0;
+        while (i < d.length) {
+            if (max_col < d[i].length)
+                max_col = d[i].length;
+            i++;
+        }
+        cols = max_col;
+        int n = rows * cols;
+        data = new double[n];
+        for (i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (d[i].length <= j)
+                    data[i * cols + j] = 0;
+                else
+                    data[i * cols + j] = d[i][j];
+            }
+        }
+
+    }
+
+    double get(int r, int c) {
+        return data[(r - 1) * cols + (c - 1)];
+    }
+
+    void set(int r, int c, double value) {
+        data[(r - 1) * cols + (c - 1)] = value;
+    }
+
+    public String toString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append("[");
+        for (int i = 0; i < rows; i++) {
+            buf.append("[");
+            for (int j = 0; j < cols; j++) {
+                buf.append(data[i * cols + j]);
+                if (j != cols - 1)
+                    buf.append(",");
+            }
+            buf.append("]");
+            if (i != rows - 1)
+                buf.append("\n");
+        }
+        buf.append("]");
+        return buf.toString();
+    }
+
+    double[][] asArray() {
+        double[][] tab = new double[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                tab[i][j] = data[i * cols + j];
+            }
+        }
+        return tab;
+    }
+
+    void reshape(int newRows, int newCols) {
+        if (rows * cols != newRows * newCols)
+            throw new RuntimeException(String.format("%d x %d matrix can't be reshaped to %d x %d", rows, cols, newRows, newCols));
+        else {
+            rows = newRows;
+            cols = newCols;
+        }
+    }
+
+    int[] shape() {
+        int[] tab = {rows, cols};
+        return tab;
+    }
+
+    Matrix add(Matrix m) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] + m.data[i * cols + j];
+            }
+        }
+        return ret;
+    }
+
+    Matrix sub(Matrix m) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] - m.data[i * cols + j];
+            }
+        }
+        return ret;
+    }
+
+    Matrix mul(Matrix m) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] * m.data[i * cols + j];
+            }
+        }
+        return ret;
+    }
+
+    Matrix div(Matrix m) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] / m.data[i * cols + j];
+            }
+        }
+        return ret;
+    }
+
+    Matrix add(double w) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] + w;
+            }
+        }
+        return ret;
+    }
+
+    Matrix sub(double w) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] - w;
+            }
+        }
+        return ret;
+    }
+
+    Matrix mul(double w) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] * w;
+            }
+        }
+        return ret;
+    }
+
+    Matrix div(double w) {
+        Matrix ret = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret.data[i * cols + j] = this.data[i * cols + j] / w;
+            }
+        }
+        return ret;
+    }
+
+    Matrix dot(Matrix m) {
+        Matrix ret = new Matrix(this.rows, m.cols);
+        int i = 0, j = 0, licz = 0, suma = 0;
+        while (j < rows) {
+            while (i < cols) {
+                suma += data[i + j * cols] * m.data[i * m.cols + licz % m.cols];
+                i++;
+            }
+            ret.data[licz] = suma;
+            suma = 0;
+            i = 0;
+            licz++;
+            if (licz % m.cols == 0)
+                j++;
+        }
+        return ret;
+    }
+
+    double frobenius() {
+        double norma = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                norma += this.data[i * cols + j] * this.data[i * cols + j];
+            }
+        }
+        norma = Math.sqrt(norma);
+        return norma;
+    }
+
+    //////// GRUPA D /////////
+
+    Matrix max(int axis){
+        if(axis==1)
+        {
+            Matrix ret = new Matrix(this.rows,1);
+            for(int i=0;i<this.rows;i++)
+            {
+                double maximum;
+                maximum=this.data[i*this.cols];
+                for(int j=0;j<this.cols;j++)
+                {
+                    if(this.data[i*this.cols+j]>maximum)
+                        maximum=this.data[i*this.cols+j];
+                }
+                ret.data[i]=maximum;
+            }
+            return ret;
+        }
+        if(axis==0)
+        {
+            Matrix ret = new Matrix(1,this.cols);
+            double[][] tab = this.asArray();
+            for(int i=0;i<this.cols;i++)
+            {
+                double maximum;
+                maximum=tab[0][i];
+                for(int j=0;j<this.rows;j++)
+                {
+                    if(tab[j][i]>maximum)
+                        maximum=tab[j][i];
+                }
+                ret.data[i]=maximum;
+            }
+            return ret;
+        }
+        if(axis==-1)
+        {
+            Matrix ret = new Matrix(1,1);
+            double[][] tab = this.asArray();
+            double maximum=tab[0][0];
+            for(int i=0;i<this.rows;i++)
+            {
+                for(int j=0;j<this.cols;j++)
+                {
+                    if(tab[i][j]>maximum)
+                        maximum=tab[i][j];
+                }
+            }
+            ret.data[0]=maximum;
+            return ret;
+        }
+        Matrix ret = new Matrix(0,0);
+        return ret;
+    }
+
+    /////////////////////////
+
+
+    public static void main(String[] args) {
+
+        double[][] tab1 = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        double[][] tab2 = {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+        };
+        Matrix wyk = new Matrix(tab1);
+        Matrix wyk1 = new Matrix(tab2);
+
+        System.out.print((wyk.max(-1)).toString());
+    }
+}
---
+++ Lab2/test/lab2/MatrixTest.java
@@ -0,0 +1,210 @@
+package lab2;
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+    @org.junit.Test
+    public void Matrix() throws Exception {
+
+    }
+
+    public void get() throws Exception {
+        double[][] tab = {{1,2,5,4,1,65463},{3,4,9}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        for(int i=1;i<=tab.length;i++)
+            for(int j=1;j<=tab[i].length;j++)
+            {
+                assertEquals(tab[i][j], actual.get(i, j), 1e-5);
+            }
+    }
+
+    @org.junit.Test
+    public void set() throws Exception {
+        lab2.Matrix actual = new lab2.Matrix(4,5);
+        for(int i=1;i<=4;i++)
+            for(int j=1;j<=5;j++)
+            {
+                actual.set(i,j,i);
+                assertEquals(i, actual.get(i,j), 1e-5);
+            }
+    }
+
+    @org.junit.Test
+    public void testToString() throws Exception {
+        double[][] tab = {{1.3,2.56,5,4.3,1,65463},{3,4.1111,9}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        String expected = "[[1.3,2.56,5.0,4.3,1.0,65463.0]\n[3.0,4.1111,9.0,0.0,0.0,0.0]]";
+        String copy = actual.toString();
+        assertEquals(expected, copy);
+        int n=0;
+        for(int i=0;i<copy.length();i++)
+            if(copy.charAt(i)==',')
+                n++;
+        assertEquals(10, n);
+        n=0;
+        for(int i=0;i<copy.length();i++)
+            if(copy.charAt(i)=='['||copy.charAt(i)==']')
+                n++;
+        assertEquals(6, n);
+    }
+
+    @org.junit.Test
+    public void asArray() throws Exception {
+        double[][] tab = {{1,2,3,3434,5466.474},{565,78,33}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        double[][] act_tab = actual.asArray();
+        double[][] exp_tab = {{1,2,3,3434,5466.474},{565,78,33,0,0}};
+        for(int i=0;i<actual.rows;i++)
+        {
+            for(int j=0;j<actual.cols;j++)
+            {
+                assertEquals(exp_tab[i][j], act_tab[i][j],1e-5);
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void reshape() throws Exception {
+        lab2.Matrix actual = new lab2.Matrix(10,15);
+        actual.reshape(3,50);
+        assertEquals(3, actual.rows,1e-5);
+        assertEquals(50, actual.cols,1e-5);
+        try {
+            actual.reshape(6,5);
+            fail("Exception wasn't thrown!");
+        }
+        catch (RuntimeException exception) {
+            assertEquals("3 x 50 matrix can't be reshaped to 6 x 5", exception.getMessage());
+        }
+    }
+
+    @org.junit.Test
+    public void shape() throws Exception {
+        lab2.Matrix actual = new lab2.Matrix(5,8);
+        int[] act = actual.shape();
+        int[] exp = {5,8};
+        assertArrayEquals(exp,act);
+    }
+
+    @org.junit.Test
+    public void add() throws Exception {
+        double[][] tab = {{88.5641556,95.2222,9999},{564.5616,666,32.32}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        assertEquals(0,(actual.add(actual.mul(-1)).frobenius()),1e-5);
+    }
+
+    @org.junit.Test
+    public void sub() throws Exception {
+        double[][] tab = {{54,65.55,87},{123.445555,1348,92}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        assertEquals(0,(actual.sub(actual).frobenius()),1e-5);
+    }
+
+    @org.junit.Test
+    public void mul() throws Exception {
+        double[][] tab = {{88.5641556,95.2222,9999},{564.5616,666,32.32}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        assertEquals(0,(actual.add(actual.mul(-1)).frobenius()),1e-5);
+    }
+
+    @org.junit.Test
+    public void div() throws Exception {
+        double[][] tab = {{54,65.55,87},{123.445555,1348,92}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        assertEquals(Math.sqrt(6),(actual.div(actual).frobenius()),1e-5);
+    }
+
+    @org.junit.Test
+    public void add1() throws Exception {
+        double[][] tab = {{54,65.55,87},{123.445555,1348,92}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        double[][] act_tab = (actual.add(20)).asArray();
+        double[][] exp_tab = {{74,85.55,107},{143.445555,1368,112}};
+        for(int i=0;i<actual.rows;i++)
+        {
+            for(int j=0;j<actual.cols;j++)
+            {
+                assertEquals(exp_tab[i][j], act_tab[i][j],1e-5);
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void sub1() throws Exception {
+        double[][] tab = {{54,65.55,87},{123.445555,1348,92}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        double[][] act_tab = (actual.sub(20)).asArray();
+        double[][] exp_tab = {{34,45.55,67},{103.445555,1328,72}};
+        for(int i=0;i<actual.rows;i++)
+        {
+            for(int j=0;j<actual.cols;j++)
+            {
+                assertEquals(exp_tab[i][j], act_tab[i][j],1e-5);
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void mul1() throws Exception {
+        double[][] tab = {{5,10,15},{6,4}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        double[][] act_tab = (actual.mul(actual)).asArray();
+        double[][] exp_tab = {{25,100,225},{36,16,0}};
+        for(int i=0;i<actual.rows;i++)
+        {
+            for(int j=0;j<actual.cols;j++)
+            {
+                assertEquals(exp_tab[i][j], act_tab[i][j],1e-5);
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void div1() throws Exception {
+        double[][] tab = {{5,10,15},{20,25}};
+        lab2.Matrix actual = new lab2.Matrix(tab);
+        double[][] act_tab = (actual.div(5)).asArray();
+        double[][] exp_tab = {{1,2,3},{4,5,0}};
+        for(int i=0;i<actual.rows;i++)
+        {
+            for(int j=0;j<actual.cols;j++)
+            {
+                assertEquals(exp_tab[i][j], act_tab[i][j],1e-5);
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void dot() throws Exception {
+        double[][] tab1 = {{5,10}};
+        double[][] tab2 = {{1},{2}};
+        lab2.Matrix actual1 = new lab2.Matrix(tab1);
+        lab2.Matrix actual2 = new lab2.Matrix(tab2);
+        double[][] act1_tab = (actual1.dot(actual2)).asArray();
+        double[][] exp1_tab = {{25}};
+        for(int i=0;i<actual1.rows;i++)
+        {
+            for(int j=0;j<actual2.cols;j++)
+            {
+                assertEquals(exp1_tab[i][j], act1_tab[i][j],1e-5);
+            }
+        }
+        double[][] act2_tab = (actual2.dot(actual1)).asArray();
+        double[][] exp2_tab = {{5,10},{10,20}};
+        for(int i=0;i<actual2.rows;i++)
+        {
+            for(int j=0;j<actual1.cols;j++)
+            {
+                assertEquals(exp2_tab[i][j], act2_tab[i][j],1e-5);
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void frobenius() throws Exception {
+        double[][] tab1 = {{2,3,4},{5,8}};
+        lab2.Matrix actual = new lab2.Matrix(tab1);
+        double act = actual.frobenius();
+        assertEquals(Math.sqrt(118), act,1e-5);
+    }
+
+}
