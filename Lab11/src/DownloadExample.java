f0b871f82ef1915c4fff9d16365ac8d08672d907
Maja Gruca <majag@mops.uci.agh.edu.pl>
2018-01-04 15:33:30 +0100

    lab11

---
+++ Lab11/src/DownloadExample.java
@@ -0,0 +1,91 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
+
+public class DownloadExample {
+
+    static int count = 0;
+
+    public static void main(String[] args)
+    {
+        sequentialDownload();
+        concurrentDownload2();
+    }
+    // lista plików do pobrania
+    static String [] toDownload = {
+            "http://home.agh.edu.pl/pszwed/wyklad-c/01-jezyk-c-intro.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/02-jezyk-c-podstawy-skladni.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/03-jezyk-c-instrukcje.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/04-jezyk-c-funkcje.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/05-jezyk-c-deklaracje-typy.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/06-jezyk-c-wskazniki.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/07-jezyk-c-operatory.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/08-jezyk-c-lancuchy-znakow.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/09-jezyk-c-struktura-programow.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/10-jezyk-c-dynamiczna-alokacja-pamieci.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/11-jezyk-c-biblioteka-we-wy.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/preprocesor-make-funkcje-biblioteczne.pdf",
+    };
+
+    static void sequentialDownload(){
+        double t1 = System.nanoTime()/1e6;
+        for(String url:toDownload){
+            new Downloader(url).run();
+        }
+        double t2 = System.nanoTime()/1e6;
+        System.out.printf(Locale.US,"t2-t1=%f\n",t2-t1);
+    }
+
+    static void concurrentDownload(){
+        double t1 = System.nanoTime()/1e6;
+        for(String url:toDownload){
+            new Downloader(url).run();
+        }
+        double t2 = System.nanoTime()/1e6;
+        System.out.printf(Locale.US,"t2-t1=%f\n",t2-t1);
+    }
+
+    static void concurrentDownload2(){
+        double t1 = System.nanoTime()/1e6;
+        while(count!=toDownload.length){
+
+            new Downloader(toDownload[count]).run();
+            Thread.yield();
+        }
+        double t2 = System.nanoTime()/1e6;
+        System.out.printf(Locale.US,"t2-t1=%f\n",t2-t1);
+    }
+
+    static class Downloader implements Runnable{
+        private final String url;
+
+        Downloader(String url){
+            this.url = url;
+        }
+
+        public void run(){
+            String [] fn = url.split("/");
+            String fileName = fn[fn.length-1];
+
+            try(InputStream in = new URL(url).openStream(); FileOutputStream out = new FileOutputStream(fileName) ){
+                for(;;){
+                    int i = in.read();
+                    if(i<0)
+                        break;
+                    out.write(i);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Done:"+fileName);
+            count++;
+        }
+
+    }
+
+}
