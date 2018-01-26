5773847c092df5ab6470323c2c4da65fd8f61af5
Maja <majag000@gmail.com>
2018-01-26 21:18:52 +0100

    balls

--- Lab14/src/BouncingBallsPanel.java
+++ Lab14/src/BouncingBallsPanel.java
@@ -2,46 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BouncingBallsPanel extends JPanel {
 
-
+    List<Ball> balls = new ArrayList<>();
     AnimationThread thread = new AnimationThread();
 
     static class Ball{
         int x;
         int y;
-        int r;
+        int r=15;
         double vx;
         double vy;
         Color color;
-        Color white=new Color(255,255,255);
         Color black=new Color(0,0,0);
 
         public void paint(Graphics g)
         {
-            g.setColor(white);
-            g.fillOval(x-r,y-r,2*r, 2*r);
-            g.setColor(black);
-            g.drawOval(x-r,y-r,2*r, 2*r);
+            Graphics2D g2d = (Graphics2D)g;
+            g2d.setColor(color);
+            g2d.fillOval(x-r,y-r,2*r, 2*r);
+            g2d.setColor(black);
+            g2d.drawOval(x-r,y-r,2*r, 2*r);
         }
 
         Ball(){
-            x=20;
-            y=20;
-            r=15;
-            vx=1;
-            vy=1;
+            Random rand = new Random();
+            x=15+rand.nextInt(270);
+            y=15+rand.nextInt(270);
+            vx=1+rand.nextDouble()*10;
+            vy=1+rand.nextDouble()*10;
+            color=new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
         }
     }
 
-    List<Ball> balls = new ArrayList<>();
-
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D)g;
         for(Ball n: balls){
-            n.paint(g2d);
+            n.paint(g);
         }
     }
 
@@ -54,6 +53,7 @@ public class BouncingBallsPanel extends JPanel {
         void safeSuspend(){
             suspend=true;
         }
+
         public void run(){
             for(;;) {
                 for (Ball n : balls) {
@@ -64,6 +64,11 @@ public class BouncingBallsPanel extends JPanel {
                     }
                 }
                 repaint();
+                try {
+                    sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 synchronized (this) {
                     try {
                         if (suspend) {
@@ -91,6 +96,11 @@ public class BouncingBallsPanel extends JPanel {
     BouncingBallsPanel(){
         setBorder(BorderFactory.createStrokeBorder(new BasicStroke(3.0f)));
         thread.start();
+        for(int i=0;i<5;i++)
+        {
+            Ball b = new Ball();
+            balls.add(b);
+        }
     }
 
     void onStart() {
